03392777858f6f27fc4ab3f3e4710f53bfc1c54e
nam_nh <namnguyen7694@gmail.com>
2020-09-24 20:55:58 +0700

    build 2409

---
+++ build/precache-manifest.39977b76c45b240657122ca7443c536c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1780a6da05d9a432fd8c0b3dc47d3058",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1af984834b7ff1fd0d6",
+    "url": "/static/css/main.4d47e2d8.chunk.css"
+  },
+  {
+    "revision": "0d86786eb9bed548fb3c",
+    "url": "/static/js/2.83d1af87.chunk.js"
+  },
+  {
+    "revision": "d938c6dad335a9c4130325da50142bf1",
+    "url": "/static/js/2.83d1af87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1af984834b7ff1fd0d6",
+    "url": "/static/js/main.c5b82523.chunk.js"
+  },
+  {
+    "revision": "8c59506e0e65e5eb8f5d",
+    "url": "/static/js/runtime-main.1a824a33.js"
+  }
+]);
